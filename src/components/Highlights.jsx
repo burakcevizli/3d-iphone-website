b5348796a5d87e8b8a4e2036a66f61a8fc137ed9
KrieqqeR <burakcevizli@gmail.com>
2024-06-17 14:42:06 +0300

    Highlts startted.

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,21 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
 
+
 const Highlights = () => {
+
+    useGSAP(()=>{
+        gsap.to('#title', {opacity:1 , y:0})
+    },[])
+
   return (
-    <div>
-      Highlights
-    </div>
+    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className='screen-max-width'>
+        <h1 id='title' className='section-heading'>
+            Get the highlights.
+        </h1>
+      </div>
+    </section>
   )
 }
 
