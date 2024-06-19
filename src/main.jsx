f7d7fcb2b5b3157b1c972b85f1e6d0e15fe41bac
KrieqqeR <burakcevizli@gmail.com>
2024-06-19 19:48:06 +0300

    Sentry is uploadded.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://087ed1a7b6b073285304188a9d146659@o4507459534716928.ingest.de.sentry.io/4507459543171152",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+   // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText:false,
+      blockAllMedia:false
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
